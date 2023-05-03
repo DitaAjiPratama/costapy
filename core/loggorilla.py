8965253222e01a526f2780620b1984c76a1325f4
Dita Aji Pratama <aji@debian-BULLSEYE-live-builder-AMD64>
2023-05-03 22:56:38 +0700

    Add logging library in core directory

---
+++ core/loggorilla.py
@@ -0,0 +1,13 @@
+import datetime
+
+def prcss(loc, msg):
+    print(f"[loggorilla][{datetime.datetime.now()}][\033[32mprcss\033[39m][\033[95m{loc}\033[39m] {msg}")
+
+def accss(loc, msg):
+    print(f"[loggorilla][{datetime.datetime.now()}][\033[32mprcss\033[39m][\033[95m{loc}\033[39m] {msg}")
+
+def fyinf(loc, msg):
+    print(f"[loggorilla][{datetime.datetime.now()}][\033[93mfyinf\033[39m][\033[95m{loc}\033[39m] {msg}")
+
+def error(loc, msg):
+    print(f"[loggorilla][{datetime.datetime.now()}][\033[31merror\033[39m][\033[95m{loc}\033[39m] {msg}")
