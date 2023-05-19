bee07d03234c01af3b0e75a14e9bd2641d28f0cb
Dita Aji Pratama <aji@debian-BULLSEYE-live-builder-AMD64>
2023-05-19 19:52:03 +0700

    change color accss to cyan and change word prcss to accss in def accss

--- core/loggorilla.py
+++ core/loggorilla.py
@@ -4,7 +4,7 @@ def prcss(loc, msg):
     print(f"[loggorilla][{datetime.datetime.now()}][\033[32mprcss\033[39m][\033[95m{loc}\033[39m] {msg}")
 
 def accss(loc, msg):
-    print(f"[loggorilla][{datetime.datetime.now()}][\033[32mprcss\033[39m][\033[95m{loc}\033[39m] {msg}")
+    print(f"[loggorilla][{datetime.datetime.now()}][\033[36maccss\033[39m][\033[95m{loc}\033[39m] {msg}")
 
 def fyinf(loc, msg):
     print(f"[loggorilla][{datetime.datetime.now()}][\033[93mfyinf\033[39m][\033[95m{loc}\033[39m] {msg}")
