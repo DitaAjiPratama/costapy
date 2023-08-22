8fa748fd62d78453bcf855e607ce9cd2299f5429
Dita Aji Pratama <aji@debian-BULLSEYE-live-builder-AMD64>
2023-08-22 20:50:12 +0700

    add max_request_body_size, socket_timeout, and timeout configuration on server.py

--- config/server.py
+++ config/server.py
@@ -7,7 +7,10 @@ update = {
     'tools.sessions.on'                 : True          ,
     'engine.autoreload.on'              : False         ,
     'request.show_tracebacks'           : False         ,
-    'error_page.403'                    : directory.erpadir(403)  ,
-    'error_page.404'                    : directory.erpadir(404)  ,
-    'error_page.500'                    : directory.erpadir(500)  ,
+    'error_page.403'                    : directory.erpadir(403)    ,
+    'error_page.404'                    : directory.erpadir(404)    ,
+    'error_page.500'                    : directory.erpadir(500)    ,
+    'server.max_request_body_size'      : 800 * 1024 * 1024         , # 800MB; Default 100MB
+    'server.socket_timeout'             : 60            , # Default 10s
+    'response.timeout'                  : 3600          , # Default 300s
 }
