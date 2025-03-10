ef0e47d7f5dcafd0cb96d8c561cd76a11e02291c
Dita Aji Pratama <ditaajipratama@protonmail.com>
2025-03-10 14:33:58 +0700

    Update carrack.js so it can put Authentication

--- static/js/carrack.js
+++ static/js/carrack.js
@@ -1,7 +1,8 @@
-function sendHttpRequest(url, method, data, callback, contentType = "multipart/form-data") {
+function sendHttpRequest(url, method, data, callback, contentType = "multipart/form-data", authorization = "") {
   var xhr = new XMLHttpRequest();
   xhr.open(method, url, true);
   xhr.setRequestHeader("Content-Type", contentType);
+  xhr.setRequestHeader("Authorization", authorization);
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
